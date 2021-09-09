fbc4788557e0bbe4a99ab505dcd2a645d9d2ec7b
venkatesh116 <69994309+venkatesh116@users.noreply.github.com>
2021-09-09 13:08:45 +0530

    Beta Version

---
+++ WebChatApp/src/test/java/com/cachatto/WebChatApp/WebChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cachatto.WebChatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
